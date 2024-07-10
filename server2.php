5ee731de927db98bd8c258d1b064f5b779bde809
Aniket Rathaur <aniketniet@gmail.com>
2024-07-10 11:21:57 +0530

    first commit

--- server2.php
+++ server2.php
@@ -3,12 +3,20 @@ require 'vendor/autoload.php';
 
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use React\EventLoop\Factory;
+use React\Socket\Server as ReactServer;
+use React\Socket\SecureServer;
 
 $logFile = '/home/ubuntu/phpchat/server.log'; 
+
 function logMessage($message) { 
     global $logFile; 
     file_put_contents($logFile, date('Y-m-d H:i:s') . " - " . $message . PHP_EOL, FILE_APPEND); 
 } 
+
 logMessage("Starting server...");  
 
 class Chat implements MessageComponentInterface {
@@ -42,18 +50,30 @@ class Chat implements MessageComponentInterface {
     }
 }
 
-$server = \Ratchet\Server\IoServer::factory(
-    new \Ratchet\Http\HttpServer(
-        new \Ratchet\WebSocket\WsServer(
+$loop = Factory::create();
+$webSock = new ReactServer('0.0.0.0:3000', $loop);
+
+// Secure the WebSocket server
+$secure_webSock = new SecureServer($webSock, $loop, [
+    'local_cert' => '/path/to/your/cert.pem', // path to your cert
+    'local_pk' => '/path/to/your/key.pem',    // path to your server private key
+    'allow_self_signed' => true,
+    'verify_peer' => false
+]);
+
+$webServer = new IoServer(
+    new HttpServer(
+        new WsServer(
             new Chat()
         )
     ),
-    4000
+    $secure_webSock,
+    $loop
 );
 
-echo "Server started at port 4000\n";
+echo "Server started at port 3000\n";
 
-$server->run();
+$loop->run();
 
 echo "Server stopped\n";
 ?>
